34fc31e945f6013059bce1842353e1aa00b8079d
Alexis Perelgritz <aperelgritz@salesforce.com>
2021-12-07 18:34:48 +0100

    Installed reselect library, refactored CartIcon and CartDropdown with memoized selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from "reselect";
+
+// Input selector
+const selectCart = state => state.cart;
+
+// Output selector
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+// Output selector
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity
+    , 0)
+);
