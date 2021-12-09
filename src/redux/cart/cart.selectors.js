075d272e6013acebc340138232a9db5cfa93b989
Alexis Perelgritz <aperelgritz@salesforce.com>
2021-12-09 18:15:49 +0100

    Added CheckoutPage and CheckoutItem components, and updated cart to use selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -10,9 +10,23 @@ export const selectCartItems = createSelector(
 );
 
 // Output selector
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+)
+
+// Output selector
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems => cartItems.reduce(
     (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity
     , 0)
+);
+
+// Output selector
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => cartItems.reduce(
+    (accumulatedPrice, cartItem) => accumulatedPrice + cartItem.quantity * cartItem.price
+      , 0)
 );
